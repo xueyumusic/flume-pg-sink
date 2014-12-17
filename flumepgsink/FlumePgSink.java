abfeb8e4d2bcb0b16f14c4c6688e13ea263fefca
root <root@spark2.(none)>
2014-12-17 14:06:14 +0800

    insert other

--- flumepgsink/FlumePgSink.java
+++ flumepgsink/FlumePgSink.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.Map;
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 import org.apache.flume.Channel;
 import org.apache.flume.Context;
@@ -30,41 +31,112 @@ public class FlumePgSink extends AbstractSink implements Configurable {
 	
 	@Override
 	public Status process() throws EventDeliveryException {
-		Status status = null;
-		
-		Channel ch = getChannel();
-		Transaction txn = ch.getTransaction();
-		txn.begin();
-		try {
-			Event event = ch.take();
-			byte[] datas = event.getBody();
-			String ds = new String(datas, "UTF-8");
-			String[] dsarr = ds.split(" ");
-			System.out.println(dsarr[0]);
-			out.println(dsarr[0]);
+        //2014-12-12 10:06:42,002 INFO  execute - HttpProxy null 0 HttpResponse 90000318622 0 3 Successful Message:null
+        //Date, log4j level, class, classic, IP, port, type, orgid, counts, forward_time, action, messages
+        Status status = null;
 
-                        //session.insert("flumepgsink.FlumePgMapper.insertContent", dsarr[0]);
-                        RawLog curobj = new RawLog();
-                        curobj.setClasstype("xueyuclasstype");
-                        curobj.setTime(new Date());
-                        session.insert("flumepgsink.FlumePgMapper.insertContent", curobj);
-                        System.out.println("###insert obj");
-                        session.commit();
+        Channel ch = getChannel();
+        Transaction txn = ch.getTransaction();
+        txn.begin();
+        try {
+                Event event = ch.take();
+                byte[] datas = event.getBody();
+                String ds = new String(datas, "UTF-8");
+	        System.out.println("##try:"+ds);
+                String[] dsarr = ds.split("\\s+");
+
+                /*for (int i=0; i<dsarr.length; i++) {
+                  System.out.println(Integer.toString(i));
+                  System.out.println(dsarr[i]);
+                }*/
+
+                if (dsarr.length >= 14 && dsarr[2].equals("INFO")) {
+
+			//session.insert("flumepgsink.FlumePgMapper.insertContent", dsarr[0]);
+			RawLog curobj = new RawLog();
+
+			curobj.setClasstype(dsarr[3]);
+			curobj.setClassic(dsarr[5]);
+			curobj.setIp(dsarr[6]);
+			curobj.setPort(Integer.valueOf(dsarr[7]));
+			curobj.setType(dsarr[8]);
+			curobj.setOrg_id(dsarr[9]);
+			curobj.setCounts(Integer.valueOf(dsarr[10]));
+			curobj.setForward_time(Integer.valueOf(dsarr[11]));
+			curobj.setAction(dsarr[12]);
+
+			SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss,SSS");
+			Date time = fmt.parse(dsarr[0]+" "+dsarr[1]);
+			curobj.setTime(time);
+
+			StringBuffer sb = new StringBuffer();
+			for (int i=13;i<dsarr.length;i++) {
+				sb.append(dsarr[i]);
+			}
+			curobj.setMessage(new String(sb));
+			session.insert("flumepgsink.FlumePgMapper.insertRawLog", curobj);
+			System.out.println("###insert obj");
 			
+			if (dsarr[8].equals("HttpRequest")) {
+			  RequestType aobj = new RequestType();
+ 			  aobj.setType(dsarr[8]);
+                          aobj.setOrg_id(dsarr[9]);
+                          aobj.setOrg_name("");
+                          aobj.setMessage(new String(sb));
+                          aobj.setTime(time);
+                          session.insert("flumepgsink.FlumePgMapper.insertHttpRequest", aobj);
+			}
+
+			session.commit();
+
 			txn.commit();
 			status = Status.READY;
-		} catch (Throwable t) {
-                        //System.out.println("###throw exception");
-                        //t.printStackTrace();
-			txn.rollback();
-			status = Status.BACKOFF;
-			if (t instanceof Error) {
-				throw (Error)t;
-			}
-		} finally {
-			txn.close();
-		}
-		return status;
+                } // end info
+                else if (dsarr.length >= 6 && dsarr[2].equals("ERROR")) {
+                  //2014-12-12 11:03:47,970 ERROR channelInactive - The channel is inactive.
+                  System.out.println("###have ERROR in log");
+                  ErrorType curobj = new ErrorType();
+                  curobj.setType("");
+                  curobj.setClass_name(dsarr[3]);
+                  StringBuffer cursb = new StringBuffer();
+                  for (int i = 5; i<dsarr.length; i++) {
+                    cursb.append(dsarr[i]);
+                  }
+                  curobj.setMessage(new String(cursb));
+                  SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss,SSS");
+                  Date time = fmt.parse(dsarr[0]+" "+dsarr[1]);
+                  curobj.setTime(time);
+
+                  session.insert("flumepgsink.FlumePgMapper.insertErrorLog", curobj);
+                  session.commit();
+                  status = Status.READY;
+                  txn.commit();
+                } else {
+                  if (dsarr.length >= 3) {
+                    System.out.println("##ds type:"+dsarr[2]);
+                    System.out.println("##ds len:"+Integer.toString(dsarr.length));
+                    if (dsarr[2] == "INFO") {
+                      System.out.println("#####yes info wield");
+                    } else {
+                      System.out.println("#####not equal info");
+                    }
+                  }
+                  System.out.println("###other situation");
+                  status = Status.BACKOFF;
+                  txn.commit();
+                }
+        } catch (Throwable t) {
+                System.out.println("###throw exception");
+                //t.printStackTrace();
+                txn.rollback();
+                status = Status.BACKOFF;
+                if (t instanceof Error) {
+                        throw (Error)t;
+                }
+        } finally {
+                txn.close();
+        }
+        return status;
 	}
 
         protected void finalize() {
@@ -88,7 +160,7 @@ public class FlumePgSink extends AbstractSink implements Configurable {
 		}
 		
 		try {
-		  String resource = "flumepgsink/mybatis-config.xml";
+		  String resource = "flumepgsink/mybatis-config-119.xml";
 		  InputStream inputmybatisstream = Resources.getResourceAsStream(resource);
 		  SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputmybatisstream);
 		  SqlSession session = sqlSessionFactory.openSession();
--- flumepgsink/compile.sh
+++ flumepgsink/compile.sh
@@ -1,2 +1,2 @@
 #javac -cp .:lib/flume-ng-core-1.5.2.jar:lib/flume-ng-configuration-1.5.2.jar:lib/flume-ng-sdk-1.5.2.jar flumepgsink/FlumePgSink.java 
-javac -cp .:lib/flume-ng-core-1.5.2.jar:lib/flume-ng-configuration-1.5.2.jar:lib/flume-ng-sdk-1.5.2.jar:flumepgsink/lib/mybatis-3.2.7.jar  flumepgsink/FlumePgSink.java
+javac -cp .:lib/flume-ng-core-1.5.2.jar:lib/flume-ng-configuration-1.5.2.jar:lib/flume-ng-sdk-1.5.2.jar:flumepgsink/lib/mybatis-3.2.7.jar  flumepgsink/FlumePgSink.java flumepgsink/RawLog.java  flumepgsink/ErrorType.java flumepgsink/RequestType.java
